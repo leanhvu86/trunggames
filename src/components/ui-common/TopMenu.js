c977558c0295cbba617cd3d7632bb1b4bec84ccd
minhnv <minhnguyenk13haui@gmail.com>
2023-05-15 00:37:31 +0700

    config multi language

--- src/components/ui-common/TopMenu.js
+++ src/components/ui-common/TopMenu.js
@@ -1,92 +1,72 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from "react-router-dom";
-import {logout} from "../../constants/userActions";
-import {connect} from "react-redux";
+import { Link } from 'react-router-dom';
+import { logout } from '../../constants/userActions';
+import { connect } from 'react-redux';
+import LocaleOptions from './Locale';
 
 class TopMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.state = {
+      click: false,
+      lang: this.props.language
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.handleLogOut= this.handleLogOut.bind(this);
-        this.state={
-            click:false,
-            lang:this.props.language
-        }
-    }
+  langChange = (e) => {
+    console.log('lang change' + e.target.name);
+  };
 
-    langChange = e => {
-        console.log('lang change' + e.target.name)
+  handleLogOut() {
+    console.log('logout');
+    this.props.logout(1);
+    window.location.href = '/login';
+  }
+
+  render() {
+    const { t } = this.props;
+
+    const { lang } = this.state;
+    const styleTop = {
+      width: '100%',
+      borderBottom: '0.5px solid #a6a6a6'
     };
 
-    handleLogOut() {
-        console.log('logout')
-        this.props.logout(1);
-        window.location.href = '/login';
-    }
-
-    render() {
-        const {t} = this.props;
-
-        const {lang} = this.state;
-        const styleTop = {
-            width: '100%',
-            borderBottom: '0.5px solid #a6a6a6'
-        }
-
-        return (
-            <div className="row navbar" style={styleTop}>
-                <div className="col-4">
-
-                </div>
-                <div className="col-8">
-                    <div className="row">
-                        <div className="col-8">
-                            {/*<select className="selectBox" onChange={this.langChange} name="lang" value={lang}>*/}
-                            {/*    <option className="optionsMenu" value="en">*/}
-                            {/*        English*/}
-                            {/*    </option>*/}
-                            {/*    <option className="optionsMenu" value="vi">*/}
-                            {/*        Việt Nam*/}
-                            {/*    </option>*/}
-                            {/*</select>*/}
-
-                        </div>
-                        <div className="col-4">
-
-                            {/*<input type="text" className="search-input " id="addEventstart" required*/}
-                            {/*       name="startDate" placeholder="Type a Game"/>*/}
-                            {/*<button >Search<i className="fa fa-search" aria-hidden="true"/>*/}
-                            {/*</button>*/}
-                            <Link to="/cart" className="cart-icon float-right" style={{width:'30px',height:'30px',position:'relative'}}>
-                                <i className="fa fa-shopping-cart fa-lg" aria-hidden="true"/>
-                                <span className="total-count">{this.props.packageCount}</span>
-                            </Link>
-                            <button onClick={this.handleLogOut}>Logout</button>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-        );
-    }
+    return (
+      <div className="row navbar" style={styleTop}>
+        <div className="col-4"></div>
+        <div className="col-8">
+          <div className="row justify-content-end text-white" style={{ gap: '1em' }}>
+            <LocaleOptions />
+            <Link to="/cart" className="cart-icon" style={{ width: '30px', height: '30px', position: 'relative' }}>
+              <i className="fa fa-shopping-cart fa-lg" aria-hidden="true" />
+              <span className="total-count">{this.props.packageCount}</span>
+            </Link>
+            <button onClick={this.handleLogOut} className="logout-btn">
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        currency: state.currency,
-        language: state.language,
-        packageCount: state.packageCount,
-    }
-}
+  return {
+    currency: state.currency,
+    language: state.language,
+    packageCount: state.packageCount
+  };
+};
 const mapDispatchToProps = (dispatch) => {
-
-    return {
-        logout: (id) => {
-            dispatch(logout(id))
-        }
+  return {
+    logout: (id) => {
+      dispatch(logout(id));
     }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(TopMenu)
+export default connect(mapStateToProps, mapDispatchToProps)(TopMenu);
--- src/index.js
+++ src/index.js
@@ -3,49 +3,53 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Games from "./components/games/Games";
-import TopSale from "./components/top-sale/TopSale";
-import Blog from "./components/blog/Blog";
-import AboutUs from "./components/about-us/AboutUs";
-import GameDetail from "./components/games/game-detail/GameDetail";
-import AuthenticateForm from "./components/user/AuthenticateForm";
-import UserProfile from "./components/user/user-profile/UserProfile";
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Games from './components/games/Games';
+import TopSale from './components/top-sale/TopSale';
+import Blog from './components/blog/Blog';
+import AboutUs from './components/about-us/AboutUs';
+import GameDetail from './components/games/game-detail/GameDetail';
+import AuthenticateForm from './components/user/AuthenticateForm';
+import UserProfile from './components/user/user-profile/UserProfile';
 import cartReducer from './reducers/cartReducer';
-import {persistStore, persistReducer} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import {createStore} from 'redux';
-import Cart from "./components/Cart";
-import {PersistGate} from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { createStore } from 'redux';
+import Cart from './components/Cart';
+import { PersistGate } from 'redux-persist/integration/react';
+import LocalesProvider from './provider/Locales';
 
 const persistConfig = {
-    key: 'root',
-    storage,
-}
+  key: 'root',
+  storage
+};
 
 const persistedReducer = persistReducer(persistConfig, cartReducer);
-let store = createStore(persistedReducer)
+let store = createStore(persistedReducer);
 
-let persist = persistStore(store)
+let persist = persistStore(store);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persist}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App/>}/>
-                    <Route path="/games" element={<Games/>}/>
-                    <Route path="/top-sale" element={<TopSale/>}/>
-                    <Route path="/blog" element={<Blog/>}/>
-                    <Route path="/about-us" element={<AboutUs/>}/>
-                    <Route path="/game-detail" element={<GameDetail/>}/>
-                    <Route path="/login" element={<AuthenticateForm/>}/>
-                    <Route path="/user-profile" element={<UserProfile/>}/>
-                    <Route path="/cart" element={<Cart/>}/>
-                </Routes>
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>,
-    document.getElementById('root'));
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persist}>
+      <LocalesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/top-sale" element={<TopSale />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/game-detail" element={<GameDetail />} />
+            <Route path="/login" element={<AuthenticateForm />} />
+            <Route path="/user-profile" element={<UserProfile />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </BrowserRouter>
+      </LocalesProvider>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
